a2bc72fc21ea845a88b4a8025647a9bf744213a3
Pablo Henrique <pablohenriqueadm94@gmail.com>
2025-08-04 10:06:28 -0300

    git commit -m ":sparkles: feat: Criando componente Grid e configurando o tailwind para receber a aplicação do css"

--- filmeskino/src/app/page.tsx
+++ filmeskino/src/app/page.tsx
@@ -1,11 +1,17 @@
 import Flex from "@/components/template/Flex";
+import Grid from "@/components/template/Grid";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <Flex col className="w-screen h-screen">
-        <p className="w-20 h-20 bg-red-200">p1</p>
-        <p className="w-20 h-20 bg-green-200">p1</p>
-    </Flex>
+    <Grid numCols={6} column numRows={3}>
+      <p className="w-20 h-20 bg-red-200">p1</p>
+      <p className="w-20 h-20 bg-green-200">p1</p>
+      <p className="w-20 h-20 bg-blue-200">p1</p>
+      <p className="w-20 h-20 bg-purple-200">p1</p>
+      <p className="w-20 h-20 bg-yellow-200">p1</p>
+      <p className="w-20 h-20 bg-orange-200">p1</p>
+      <p className="w-20 h-20 bg-orange-200">p1</p>
+    </Grid>
   );
 }
