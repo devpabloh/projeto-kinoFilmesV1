822007bb2c06b408cc93cc9ae8056f58cd72d958
Pablo Henrique <pablohenriqueadm94@gmail.com>
2025-08-05 07:33:56 -0300

    git commit -m ":sparkles: feat: Botões de navegação e estrutura inicial do componente Carrossel"

--- filmeskino/src/app/page.tsx
+++ filmeskino/src/app/page.tsx
@@ -2,19 +2,14 @@ import Flex from "@/components/template/Flex";
 import Grid from "@/components/template/Grid";
 import Container from "@/components/template/Container";
 import Wrap from "@/components/template/Wrap";
+import Carrossel from "@/components/template/Carrossel";
 
 export default function Home() {
   return (
-    <div className="w-96 bg-yellow-500">
-      <Container>
-        <p className="w-20 h-20 bg-red-200">p1</p>
-        <p className="w-20 h-20 bg-green-200">p1</p>
-        <p className="w-20 h-20 bg-blue-200">p1</p>
-        <p className="w-20 h-20 bg-purple-200">p1</p>
-        <p className="w-20 h-20 bg-yellow-200">p1</p>
-        <p className="w-20 h-20 bg-orange-200">p1</p>
-        <p className="w-20 h-20 bg-orange-200">p1</p>
-      </Container>
-    </div>
+    <Carrossel slideAutomatico>
+      <p>Texto 1</p>
+      <p>Texto 2</p>
+      <p>Texto 3</p>
+    </Carrossel>
   );
 }
