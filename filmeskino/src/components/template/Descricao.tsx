13c7ebb7cee319f543c29c09448aa15b6b86f2ef
Pablo Henrique <pablohenriqueadm94@gmail.com>
2025-08-11 11:35:57 -0300

    git commit -m ":sparkles: feat: Componente de descricao"

---
+++ filmeskino/src/components/template/Descricao.tsx
@@ -0,0 +1,12 @@
+import mergeClass from "@/utils/mergeClasses";
+
+interface DescricaoProps{
+    texto: string
+    className?: string
+}
+
+export default function Descricao({texto, className}:DescricaoProps){
+    return(
+        <p className={mergeClass(`mt-2 text-zinc-400 tracking-wide leading-relaxed text-sm lg:text-lg text-justify`)}>{texto}</p>
+    )
+}
