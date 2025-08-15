5c277199b0f0c8b5c9afae9439bf6843a61550fb
Pablo Henrique <pablohenriqueadm94@gmail.com>
2025-08-15 14:16:26 -0300

    git commit -m ":sparkles: feat: Criando componente cabeçalho da página"

--- filmeskino/src/components/template/Cabecalho.tsx
+++ filmeskino/src/components/template/Cabecalho.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { PopcornIcon } from "@phosphor-icons/react";
 import Flex from "./Flex";
 import Wrap from "./Wrap";
@@ -6,12 +7,19 @@ import Link from "next/link";
 export default function Cabecalho(){
     return(
         <Wrap className="bg-neutral-950">
-            <header className="p-4">
+            <header className="p-4 px-32">
                 <Flex className="justify-between">
-                    <Link href="/">
+                    <Link href="/"
+                    className={`font-bold px-4 py-2 bg-red-kino flex gap-1 items-center justify-center rounded-lg`}>
                         <PopcornIcon/>
+                        Kino
                     </Link>
-                    <Link href=""></Link>
+                    <Link href="https://www.themoviedb.org/"
+                    className={'font-bold px-4 hover:brightness-75'}
+                    target="_blank" rel="noopener noferrer"
+                    >
+                        The Movie DB
+                    </Link>
                 </Flex>
             </header>
         </Wrap>
