ac9b68ef831d53265a465314295e914a577fcc29
Pablo Henrique <pablohenriqueadm94@gmail.com>
2025-08-15 14:14:10 -0300

    git commit -m ":sparkles: feat: Criando a typagem global para dados dos filmes que serão recebidos da API"

---
+++ filmeskino/types.d.ts
@@ -0,0 +1,9 @@
+interface Filme{
+    id: string;
+    titulo: string;
+    descricao: string;
+    linkImagemFundo: string;
+    linkImagemPoster: string;
+    nota: number;
+    dataLancamento: Date;
+}
