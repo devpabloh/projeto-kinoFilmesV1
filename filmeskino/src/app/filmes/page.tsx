d659382937804b3e1a6f76997b720a60a630fe68
Pablo Henrique <pablohenriqueadm94@gmail.com>
2025-08-20 11:51:57 -0300

    ":sparkles: feat: criando componente CardFilmes"

--- filmeskino/src/app/filmes/page.tsx
+++ filmeskino/src/app/filmes/page.tsx
@@ -4,6 +4,7 @@ import useMovieAPI from "@/hooks/useMovieAPI";
 import Wrap from "@/components/template/Wrap";
 import Carrossel from "@/components/template/Carrossel";
 import CardFilmeEmDestaque from "@/components/Filmes/CardFilmeEmDestaque";
+import ListaDeFilmes from "@/components/Filmes/ListaDeFilmes";
 
 export default function Filmes(){
     const [filmes, setFilmes] = useState<Filme[]>([])
@@ -19,6 +20,7 @@ export default function Filmes(){
                     return <CardFilmeEmDestaque filme={filme} key={filme.id}/>
                 })}
             </Carrossel>
+            <ListaDeFilmes filmes={filmes} titulo="Últimos Filmes"/>
         </Wrap>
     )
 }
