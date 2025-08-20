30238995ffbb2207b0788f8297823cbea042ffd6
Pablo Henrique <pablohenriqueadm94@gmail.com>
2025-08-20 11:52:30 -0300

    ":sparkles: feat: criando componente Nota"

---
+++ filmeskino/src/components/Filmes/Nota.tsx
@@ -0,0 +1,17 @@
+import {StarIcon} from "@phosphor-icons/react"
+import Flex from "../template/Flex"
+import mergeClasses from "@/utils/mergeClasses"
+
+interface NotaProps{
+    nota: number,
+    grande?: boolean
+}
+
+export default function Nota({nota,grande}:NotaProps){
+    return(
+        <Flex className="mt-2">
+            <StarIcon weight="fill" className={mergeClasses(``)}/>
+            <span className={mergeClasses("font-semibold",{"text-xl":grande})}>{nota.toFixed(1)}/10</span>
+        </Flex>
+    )
+}
