000d02d39709345ae892302237b022b2895588f8
Pablo Henrique <pablohenriqueadm94@gmail.com>
2025-08-28 06:41:04 -0300

    :lipstick: feat: Adicionando estilização do framer-motion

--- filmeskino/src/app/filmes/[id]/page.tsx
+++ filmeskino/src/app/filmes/[id]/page.tsx
@@ -1,7 +1,22 @@
+'use client'
+import CardFilmeDetalhado from "@/components/Filmes/CardFilmeDetalhado"
+import Wrap from "@/components/template/Wrap"
+import useMovieAPI from "@/hooks/useMovieAPI"
+import {useParams} from "next/navigation"
+import { useEffect, useState } from "react"
+
 export default function Filme(){
+    const {id} = useParams()
+    const [detalhesFilme, setDetalhesFilmers] = useState<FilmeDetalhado | null>(null)
+    const {getFilmeDetalhado} = useMovieAPI()
+
+    useEffect(()=>{
+        getFilmeDetalhado(String(id)).then(setDetalhesFilmers)
+    },[])
+
     return(
-        <div className="text-6xl font-bold font-white">
-            Página de um filme
-        </div>
+        <Wrap>
+            {detalhesFilme && <CardFilmeDetalhado filme={detalhesFilme}/>}
+        </Wrap>
     )
 }
--- filmeskino/src/components/Filmes/ListaDeFilmes.tsx
+++ filmeskino/src/components/Filmes/ListaDeFilmes.tsx
@@ -1,7 +1,10 @@
+import Link from "next/link"
 import Container from "../template/Container"
 import Grid from "../template/Grid"
 import Titulo from "../template/Titulo"
 import CardFilme from "./CardFilme"
+import { useState } from "react"
+import { AnimatePresence, motion } from "framer-motion"
 
 interface ListaDeFilmesProps{
     filmes: Filme[],
@@ -11,13 +14,41 @@ interface ListaDeFilmesProps{
 }
 
 export default function ListaDeFilmes({filmes,titulo,className,tituloPequeno}:ListaDeFilmesProps){
+    const [indiceSelecionado, SetIndiceSelecionado] = useState<null | number>(null)
+
     return(
         <Container className={className}>
             <Titulo className="pl-2" texto={titulo} pequeno alinhar="center"/>
             <Grid className="md:grid-cols-2 lg:grid-cols-3 py-5">
-                {filmes.map((filme)=>{
+                {filmes.map((filme, indice)=>{
                     return(
-                        <CardFilme filme={filme}/>
+                        <Link 
+                            href={`/filmes/${filme.id}`} 
+                            key={filme.id} 
+                            className="relative group block p-2 h-full w-full" 
+                            onMouseEnter={() => SetIndiceSelecionado(indice)}
+                            onMouseLeave={() => SetIndiceSelecionado(null)}
+                        >
+                            <AnimatePresence>
+                                {indiceSelecionado === indice && (
+                                <motion.span
+                                    className={`absolute inset-0 h-full w-full bg-red-kino block  rounded-3xl`}
+                                    layoutId="hoverBackground"
+                                    initial={{ opacity: 0 }}
+                                    animate={{
+                                    opacity: 1,
+                                    transition: { duration: 0.15 },
+                                    }}
+                                    exit={{
+                                    opacity: 0,
+                                    transition: { duration: 0.15, delay: 0.2 },
+                                    }}
+                                />
+                                )}
+                            </AnimatePresence>
+
+                            <CardFilme filme={filme}/>
+                        </Link>
                     )
                 })}
             </Grid>
