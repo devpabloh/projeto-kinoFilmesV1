a201d57e5aab730b6e2e23b82f1bf928a1abdf85
Pablo Henrique <pablohenriqueadm94@gmail.com>
2025-08-30 20:01:53 -0300

    git commit -m ":sparkles: feat: Página de ator por id"

--- filmeskino/src/app/ator/[id]/page.tsx
+++ filmeskino/src/app/ator/[id]/page.tsx
@@ -1,7 +1,24 @@
+'use client'
+import {useParams} from "next/navigation"
+import useMovieAPI from "@/hooks/useMovieAPI"
+import { useState, useEffect } from "react"
+import Wrap from "@/components/template/Wrap"
+import Container from "@/components/template/Container"
+
 export default function Ator(){
+    const [ator, setAtor] = useState<AtorDetalhado | null>(null)
+    const {id} = useParams()
+    const {getAtorDetalhado} = useMovieAPI();
+
+    useEffect(()=>{
+        getAtorDetalhado(String(id)).then(setAtor)
+    },[])
+
     return(
-        <div className="text-6xl font-bold font-white">
-            Página de um ator
-        </div>
+        <Wrap>
+            <Container className="mt-32 md:mt-44 min-h-96 w-full">
+                <div>{JSON.stringify(ator)}</div>
+            </Container>
+        </Wrap>
     )
 }
