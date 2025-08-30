031f472cbd5ecbf23d64f6ef7b91fd61d5c01137
Pablo Henrique <pablohenriqueadm94@gmail.com>
2025-08-30 20:02:11 -0300

    git commit -m ":sparkles: feat: Página de filmes por id"

--- filmeskino/src/app/filmes/[id]/page.tsx
+++ filmeskino/src/app/filmes/[id]/page.tsx
@@ -1,5 +1,7 @@
 'use client'
 import CardFilmeDetalhado from "@/components/Filmes/CardFilmeDetalhado"
+import Elenco from "@/components/Filmes/Elenco"
+import SugestoesFilmes from "@/components/Filmes/SugestoesFilmes"
 import Wrap from "@/components/template/Wrap"
 import useMovieAPI from "@/hooks/useMovieAPI"
 import {useParams} from "next/navigation"
@@ -17,6 +19,8 @@ export default function Filme(){
     return(
         <Wrap>
             {detalhesFilme && <CardFilmeDetalhado filme={detalhesFilme}/>}
+            {detalhesFilme?.atores && <Elenco elenco={detalhesFilme.atores}/>}
+            <SugestoesFilmes idFilme={String(id)}/>
         </Wrap>
     )
 }
