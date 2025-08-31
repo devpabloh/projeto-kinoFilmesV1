342568cd9b49824591ee6e3be940e3037127ef93
Pablo Henrique <pablohenriqueadm94@gmail.com>
2025-08-31 15:17:50 -0300

    :sparkles: feat: Criando componente de loading

---
+++ filmeskino/src/app/loading.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import Wrap from "@/components/template/Wrap";
+import { CircleNotchIcon } from "@phosphor-icons/react";
+
+
+export default function Carregando(){
+    return (
+        <Wrap className="absolute w-full top-1/2 bg-yellow-500">
+            <CircleNotchIcon size={80} weight="bold" className="text-white animate-spin m-auto"/>
+        </Wrap>
+    )
+}
