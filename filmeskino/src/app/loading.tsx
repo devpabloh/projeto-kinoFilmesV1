95ce1ad89231eae379d8d60cb5936844ce7538f8
Pablo Henrique <pablohenriqueadm94@gmail.com>
2025-08-31 20:34:27 -0300

    :lipstick: feat: Estilizando o componente de Loading

--- filmeskino/src/app/loading.tsx
+++ filmeskino/src/app/loading.tsx
@@ -6,7 +6,7 @@ import { CircleNotchIcon } from "@phosphor-icons/react";
 
 export default function Carregando(){
     return (
-        <Wrap className="absolute w-full top-1/2 bg-yellow-500">
+        <Wrap className="absolute w-full top-1/2">
             <CircleNotchIcon size={80} weight="bold" className="text-white animate-spin m-auto"/>
         </Wrap>
     )
