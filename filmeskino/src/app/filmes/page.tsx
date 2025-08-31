69565ea3dfb3d0edf90a6978a26ab88f9f7b7f66
Pablo Henrique <pablohenriqueadm94@gmail.com>
2025-08-31 15:27:26 -0300

    :boom: fix: Transformando o componente pagina filmes de lado do cliente para lado do servidor

--- filmeskino/src/app/filmes/page.tsx
+++ filmeskino/src/app/filmes/page.tsx
@@ -1,23 +1,18 @@
-'use client'
-import {useEffect, useState} from "react";
 import useMovieAPI from "@/hooks/useMovieAPI";
 import Wrap from "@/components/template/Wrap";
 import Carrossel from "@/components/template/Carrossel";
 import CardFilmeEmDestaque from "@/components/Filmes/CardFilmeEmDestaque";
 import ListaDeFilmes from "@/components/Filmes/ListaDeFilmes";
 
-export default function Filmes(){
-    const [filmes, setFilmes] = useState<Filme[]>([])
+export default async function Filmes(){
     const {getUltimosFilmes} = useMovieAPI();
+    const filmes:Filme[] = await getUltimosFilmes()
 
-    useEffect(()=>{
-        getUltimosFilmes().then(setFilmes)
-    },[])
     return(
         <Wrap>
             <Carrossel slideAutomatico>
-                {filmes.map((filme )=>{
-                    return <CardFilmeEmDestaque filme={filme} key={filme.id}/>
+                {filmes.map((filme, index )=>{
+                    return <CardFilmeEmDestaque filme={filme} key={filme.id} priority={index === 0}/>
                 })}
             </Carrossel>
             <ListaDeFilmes filmes={filmes} titulo="Últimos Filmes"/>
