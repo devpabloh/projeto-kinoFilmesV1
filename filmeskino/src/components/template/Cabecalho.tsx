8781abf30ea212d94dae8d43d18f59cb8a4cb9e1
Pablo Henrique <pablohenriqueadm94@gmail.com>
2025-08-31 20:36:40 -0300

    :lipstick: feat: Estilizando o componente de cabecalho

--- filmeskino/src/components/template/Cabecalho.tsx
+++ filmeskino/src/components/template/Cabecalho.tsx
@@ -7,8 +7,8 @@ import Link from "next/link";
 export default function Cabecalho(){
     return(
         <Wrap className="bg-neutral-950">
-            <header className="p-4 px-32">
-                <Flex className="justify-between">
+            <header className="p-4 px-auto">
+                <Flex className="justify-around">
                     <Link href="/"
                     className={`font-bold px-4 py-2 bg-red-kino flex gap-1 items-center justify-center rounded-lg`}>
                         <PopcornIcon/>
