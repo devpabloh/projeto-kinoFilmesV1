661b92c325c99c5f6e1eacd55c03b51a66dde085
Pablo Henrique <pablohenriqueadm94@gmail.com>
2025-08-31 20:35:39 -0300

    :lipstick: feat: Estilizando o componente de DetalhesAtor

--- filmeskino/src/components/ator/DetalhesAtor.tsx
+++ filmeskino/src/components/ator/DetalhesAtor.tsx
@@ -23,12 +23,12 @@ export default function DetalhesAtor({ator}:DetalhesAtorProps){
     return(
         <Flex col className={`bg-zinc-900 rounded-lg w-full justify-between items-center pt-16 md:pt-20 lg:pt-26`}>
             <Titulo texto={Ator.name} alinhar="center"/>
-            <Flex className="gap-5 flex-wrap font-semibold">
+            <Flex className="gap-5 flex-wrap font-semibold text-balance text-center">
                 {ator.genero && (<span>Gênero {ator.genero}</span>)}
                 {ator.dataNascimento && (<span>Data de Nascimento {new Intl.DateTimeFormat("pt-BR").format(ator.dataNascimento)}</span>)}
                 {ator.localNascimento && (<span>Local de Nascimento {ator.localNascimento}</span>)}
             </Flex>
-            <Flex col className="p-3 flex-1">
+            <Flex col className="lg:p-3 flex-1">
                 {ator.biografia ? (
                     <>
                         <Titulo texto="Biografia" pequeno alinhar="center" className="mb-0"/>
